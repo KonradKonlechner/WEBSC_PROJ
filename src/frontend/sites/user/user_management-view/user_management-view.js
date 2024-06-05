a860501453d6ecc7c87e367b9e51dc4db45a8731
zaschaa <wi22b025@technikum-wien.at>
2024-06-05 18:30:34 +0200

    feature: add modal box to user management

--- src/frontend/sites/user/user_management-view/user_management-view.js
+++ src/frontend/sites/user/user_management-view/user_management-view.js
@@ -11,9 +11,10 @@ function getAllUsers() {
         data: {method: "getAllUsers", param: null},
         dataType: "json"
     }).done(function (response) {
-        response.forEach(function(user) {
-           appendUser(user)
+        response.forEach(function (user) {
+            appendUser(user)
         })
+        closeAllUserInfos()
     }).fail(function () {
         console.log("Request failed!");
         alert("Es tut uns Leid, auf unserer Seite scheint es zu einem Fehler gekommen zu sein. " +
@@ -21,19 +22,33 @@ function getAllUsers() {
     });
 }
 
-function userClicked(user) {
-    console.log(user.username + "has been clicked.")
+function openUserInfo(user) {
+    const isAlreadyOpen = $("#userInfo"+user.username).hasClass("closed");
+    closeAllUserInfos();
+    if (isAlreadyOpen) {
+        $("#userInfo" + user.username)
+            .removeClass("closed")
+            .css("display", "block");
+    }
+}
+
+function closeAllUserInfos() {
+    $(".userInfo")
+        .addClass("closed")
+        .css("display", "none");
 }
 
 function appendUser(user) {
     $("#users")
         .append(
-            $("<div/>", {
+            $("<button/>", {
                 id: user.username,
                 class: "userClickBox",
                 type: "user"
             })
-                .click(function(e) {userClicked(user)})
+                .click(function (e) {
+                    openUserInfo(user)
+                })
                 .append(
                     $("<div/>", {
                         name: user.username,
@@ -47,6 +62,11 @@ function appendUser(user) {
                         name: user.lastname,
                         text: user.lastname
                     }),
+                    $("<div/>", {
+                        class: "userInfo",
+                        id: "userInfo" + user.username,
+                        text: "Hier sollten die Userdaten stehen"
+                    })
                 )
         )
 }
