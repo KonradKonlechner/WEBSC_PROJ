f427f1706c0f741b30ca784da6e025dc980cc2ba
zaschaa <wi22b025@technikum-wien.at>
2024-06-15 13:26:02 +0200

    refactor: remove unused imports

--- src/backend/order/persistence/OrderManagementSystem.php
+++ src/backend/order/persistence/OrderManagementSystem.php
@@ -2,26 +2,19 @@
 
 namespace order;
 
-use product\ProductManagementSystem;
-use product\ProductRepository;
-
 require_once '../model/Order.php';
-require_once '../../product/model/Product.php';
 require_once 'OrderRepository.php';
 require_once 'PositionRepository.php';
-require_once '../../product/persistence/ProductRepository.php';
 
 class OrderManagementSystem
 {
     private OrderRepository $orderRepo;
-    private ProductRepository $productRepo;
     private PositionRepository $orderPositionRepo;
 
     public function __construct()
     {
         $this->orderRepo = new OrderRepository();
         $this->orderPositionRepo = new PositionRepository();
-        $this->productRepo = new ProductRepository();
     }
 
     public function addProductToShoppingCart($productId)
